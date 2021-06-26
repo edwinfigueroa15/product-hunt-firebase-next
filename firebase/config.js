d775d5937cc4802b3ec371b8147dbdc0a3e8fd5b
Edwin <edwinfigueroa15@hotmail.com>
2021-06-26 17:59:17 -0500

    Configurando Firebase

---
+++ firebase/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCzheAiRCcUMcwzkYUJJTQ62pJNKQYgtuQ",
+    authDomain: "produc-hunt-d7ef4.firebaseapp.com",
+    projectId: "produc-hunt-d7ef4",
+    storageBucket: "produc-hunt-d7ef4.appspot.com",
+    messagingSenderId: "535809689752",
+    appId: "1:535809689752:web:03fb8d593e9b62528468ed"
+}
+
+export default firebaseConfig
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,16 @@
+import App from 'next/app'
+import firebase, { FirebaseContext } from '../firebase'
 import '../styles/globals.css'
 import Layout from '../components/layouts/Layout'
 
 function MyApp({ Component, pageProps }) {
     return (
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
+        <FirebaseContext.Provider value={{ firebase }}>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </FirebaseContext.Provider>
     )
-  
 }
 
 export default MyApp
