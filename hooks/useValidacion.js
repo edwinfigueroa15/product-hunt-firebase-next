16894ada88d7d4a3a1edb7bba5ddf0bc4ddb169c
Edwin <edwinfigueroa15@hotmail.com>
2021-06-26 17:41:04 -0500

    Creacion de crear cuenta

---
+++ hooks/useValidacion.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react'
+
+const useValidacion = (initialState, validar, fn) => {
+    const [valores, guardarValores] = useState(initialState)
+    const [errores, guardarErrores] = useState({})
+    const [submitform, guardarSubmitForm] = useState(false)
+
+    useEffect( () => {
+        if(submitform) {
+            const noErrores = Object.keys(errores).length === 0
+
+            if(noErrores) {
+                fn() // Funcion que se ejecuta en el componente
+            }
+
+            guardarSubmitForm(false)
+        }
+    }, [])
+
+    const handleChange = e => {
+        guardarValores({
+            ...valores,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleBlur = () => {
+        const erroresValidacion = validar(valores)
+        guardarErrores(erroresValidacion)
+    }
+
+    const handleSubmit = () => {
+        e.preventDefault()
+        const erroresValidacion = validar(valores)
+        guardarErrores(erroresValidacion)
+        guardarSubmitForm(true)
+    }
+
+    return { valores, errores, submitform, handleChange, handleBlur, handleSubmit }
+}
+
+export default useValidacion
--- pages/auth/crear-cuenta.js
+++ pages/auth/crear-cuenta.js
@@ -1,11 +1,87 @@
-import React from 'react';
- 
+import React, { useState } from 'react';
+import { css } from '@emotion/core';
+import Router from 'next/router';
+import { Formulario, Campo, InputSubmit, Error } from '../../components/ui/Formulario';
+
+// validaciones
+import useValidacion from '../../hooks/useValidacion';
+import validarCrearCuenta from '../../validacion/validarCrearCuenta';
+
+const initialState = {
+    nombre: '',
+    email: '',
+    password: ''
+}
+    
 const CrearCuenta = () => {
+    const [ error, guardarError] = useState(false);
+    const { valores, errores, handleChange, handleBlur, handleSubmit } = useValidacion(initialState, validarCrearCuenta, crearCuenta);
+    const { nombre, email, password } = valores;
+
+    async function crearCuenta() {
+        try {
+            await firebase.registrar(nombre, email, password);
+            Router.push('/')
+
+        } catch (error) {
+            console.error('Hubo un error al crear el usuario ', error.message);
+            guardarError(error.message)
+        }
+    }
+
     return (
         <div>
-            Crear Cuenta
-        </div>
-    )
-}
+            <h1 css={css`text-align: center; margin-top: 5rem;`}>Crear Cuenta</h1>
+            <Formulario onSubmit={handleSubmit} noValidate>
 
-export default CrearCuenta
+                <Campo>
+                    <label htmlFor="nombre">Nombre</label>
+                    <input
+                        type="text"
+                        id="nombre"
+                        placeholder="Tu Nombre"
+                        name="nombre"
+                        value={nombre}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                    />
+                </Campo>
+                {errores.nombre && <Error>{errores.nombre}</Error>}
+
+                <Campo>
+                    <label htmlFor="email">Email</label>
+                    <input
+                        type="email"
+                        id="email"
+                        placeholder="Tu Email"
+                        name="email"
+                        value={email}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                    />
+                </Campo>
+                {errores.email && <Error>{errores.email}</Error>}
+
+                <Campo>
+                    <label htmlFor="password">Password</label>
+                    <input
+                        type="password"
+                        id="password"
+                        placeholder="Tu Password"
+                        name="password"
+                        value={password}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                    />
+                </Campo>
+                {errores.password && <Error>{errores.password}</Error>}
+
+                {error && <Error>{error} </Error>}
+                <InputSubmit type="submit" value="Crear Cuenta" />
+
+            </Formulario>
+        </div>
+    );
+};
+
+export default CrearCuenta;
